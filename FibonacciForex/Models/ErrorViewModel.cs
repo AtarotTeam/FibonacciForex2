02109460c9e4128cabb1aa58fa1ae9d323577320
TourajOstovari <tornet656@gmail.com>
2019-01-11 13:39:35 +0330

    Add project files.

---
+++ FibonacciForex/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FibonacciForex.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
